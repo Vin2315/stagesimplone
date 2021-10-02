509219bdad5d27d749203065911c8e2c2fab163d
Vin2315-creator <viniciovillalta@gmail.com>
2021-10-02 17:42:14 +0200

    ok

--- js/main.js
+++ js/main.js
@@ -1,8 +1,7 @@
-const formulaire = document.getElementById('test'); //accede al formulario
+const formulaire = document.getElementById('formulaire_test'); //accede al formulario
 
-const input = document.getElementById("q1-r1");
+const input = document.getElementById('q1-r1');
 const error = document.getElementsByTagName('input');
-const form = document.getElementById('id-de-ton-formulaire'); form.addEventListener('submit', validerFunction);
 
 function recupererReponse(id) {
 	// On recupere le bloc qui contient toutes les inputs de la question 1
@@ -15,25 +14,25 @@ function recupererReponse(id) {
 	const inputElementArray = Object.values(inputElementcollection);
 
 	// On trouve l'input avec un status 'checked' => https://www.w3schools.com/jsref/jsref_find.asp
-	const checkedInput = inputElementArray.find(inputElement => inputElement.checked === true); 
+	const checkedInput = inputElementArray.find(
+		(inputElement) => inputElement.checked === true,
+	);
 
 	// Retourne la valeur de l'input checké, et sinon null
-	if (checkedInput) { 
+	if (checkedInput) {
 		return checkedInput.value;
 	} else {
 		return null;
 	}
 }
-
 
 function recupererReponsesFormulaire() {
-	
-	const reponses = {}; 
-	reponses.question1 = recupererReponse("question_1"); 
-	return reponses; 
-
+	const reponses = {};
+	for (let index = 1; index < 14; index++) {
+		reponses['question_' + index] = recupererReponse('question_' + index);
+	}
+	return reponses;
 }
-
 
 function recupererReponse(id) {
 	const element = document.getElementById(id);
@@ -62,7 +61,6 @@ function recupererReponse(id) {
 		return null;
 	}
 }
-
 
 function reponseformulaire() {
 	overall.addEventListener('click', function () {
@@ -89,30 +87,27 @@ function reponseformulaire() {
 			overall.indeterminate = true;
 		}
 	}
-
 }
 
-
-// remplace 
+// remplace
 function replacer(str, p1, p2, offset, s) {
-	return str + " - " + p1 + " , " + p2;
+	return str + ' - ' + p1 + ' , ' + p2;
 }
-var newString = "XXzzzz".replace(/(X*)(z*)/, replacer);
+var newString = 'XXzzzz'.replace(/(X*)(z*)/, replacer);
 console.log(newString); // XXzzzz - XX , zzzz
-
-
-
 
 function replacer(au, en, àla, à) {
-	return str + " - " + p1 + " , " + p2;
+	return str + ' - ' + p1 + ' , ' + p2;
 }
-var newString = "XXzzzz".replace(/(X*)(z*)/, replacer);
+var newString = 'XXzzzz'.replace(/(X*)(z*)/, replacer);
 console.log(newString); // XXzzzz - XX , zzzz
 
-
-
-
-
+function onSubmit(event) {
+	event.preventDefault();
+	const resultat = recupererReponsesFormulaire();
+	console.log(resultat);
+}
+formulaire.addEventListener('submit', onSubmit);
 
 // //function(){
 
@@ -186,6 +181,3 @@ console.log(newString); // XXzzzz - XX , zzzz
 // formulaire.addEventListener('submit', validerfuntion);
 
 // //}
-
-
-
