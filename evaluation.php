06b71c109f15e420e449d3f7b50e3e312e5ed768
Thibault RIZZO <thibault.rizzo@outlook.fr>
2021-11-17 09:12:26 +0100

    Ajout de la validation du formulaire et requete vers evaluation.php

---
+++ evaluation.php
@@ -0,0 +1,12 @@
+<?php
+   if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+       // TODO: Enregistrer les resultats
+       // Mettre en place une table 'evaluations' qui va contenir une colonne pour chaque reponse
+       // et aussi la foreign key/reference de l'utilisateur qui a passe le test
+       // TODO: corriger les reponses
+       
+       // TODO: renvoyer les corrections
+      echo "90%" // Resultat
+      exit();
+   }
+?>
--- js/main.js
+++ js/main.js
@@ -70,10 +70,33 @@ function onSubmit(event) {
 
 	const estValide = validerFormulaire();
 	if (estValide) {
-		console.log(resultat);
+		// Envoyer une requete au serveur
+		envoyerResultat(resultat);
 	} else {
-		alert("Votre formulaire n'est pas valide");
+		// Recuperer la liste des questions qui manquent
+		const questionsManquantes = Object.entries(resultat).filter(question => {
+			const valeurQuestion = question[1];
+			return valeurQuestion === null;
+		});
+		// Je recupere pour chaque [label, valeur] de chaque question son label, puis a la 10e position le chiffre de la question
+		const numerosQuestionsManquantes = questionsManquantes.map(question => question[0].substring(9));
+		alert(`Votre formulaire n'est pas valide, il manque les questions suivantes: ${numerosQuestionsManquantes.join(',')}`);
+		// Rediriger l'utilisateur vers la premiere question qui manque
+		goToCard(numerosQuestionsManquantes[0])
+
 	}
+}
+
+function envoyerResultat(resultat) {
+	console.log(resultat);
+
+	fetch('evaluation.php', {
+		method: "POST",
+		body: resultat
+	}).then(response => {
+		// TODO: Rediriger vers page de succes
+		console.log(response.text())
+	})
 }
 
 function init() {
@@ -172,81 +195,10 @@ function goToCard(numeroCard) {
 }
 
 function validerFormulaire() {
-	///tous les question son ok, metre en rouje les pas valide
-	return true;
+	///tous les questions son ok, mettre en rouge les pas valides
+	const resultat = recupererReponsesFormulaire();
+	return Object.values(resultat).every(valeur => valeur !== null);
 }
+
 // Demarrage de l'app
 init();
-
-// //function(){
-
-// var formulaire = document.getElementById('champ-formulaire');
-// Nom = formulaire.nom;
-// prenom = formulaire.prenom;
-// téléphone = formulaire.tel;
-// email = formulaire.mail;
-// rédaction = formulaire.message;
-
-// function validerNom(e) {
-// 	if (nom.value == '' || nom.value == null) {
-// 		error.style.display = 'block';
-// 		error.innerHTML += '<li> Complete votre Nom</li>';
-// 		console.log('Merci pour complete votre Nom');
-
-// 		e.preventDefault();
-// 	} else {
-// 		error.style.display = 'none';
-// 	}
-// }
-
-// function validerPrenom(e) {
-// 	if (prenom.value == '' || prenom.value == null) {
-// 		error.style.display = 'block';
-// 		error.innerHTML += '<li> Complete votre Prenom</li>';
-// 		console.log('Merci pour complete votre Prenom');
-
-// 		e.preventDefault();
-// 	} else {
-// 		error.style.display = 'none';
-// 	}
-// }
-
-// function validerTel(e) {
-// 	if (tel.value == '' || tel.value == null) {
-// 		error.style.display = 'block';
-// 		error.innerHTML += '<li> Complete votre numéro de téléphone </li>';
-// 		console.log('Merci pour complete votre numéro de téléphone');
-
-// 		e.preventDefault();
-// 	} else {
-// 		error.style.display = 'none';
-// 	}
-// }
-
-// function validerEmail(e) {
-// 	if (mail.value == '' || mail.value == null) {
-// 		error.style.display = 'block';
-// 		error.innerHTML += '<li>Merci pour complete votre email </li>';
-// 		console.log('Merci pour complete votre email');
-
-// 		e.preventDefault();
-// 	} else {
-// 		error.style.display = 'none';
-// 	}
-// }
-
-// function validarRédaction(e) {
-// 	if (message.value == '' || message.value == null) {
-// 		error.style.display = 'block';
-// 		error.innerHTML += '<li>Merci pour complete votre email </li>';
-// 		console.log('Redacte votre text la totalite de 50');
-
-// 		e.preventDefault();
-// 	} else {
-// 		error.style.display = 'none';
-// 	}
-// }
-
-// formulaire.addEventListener('submit', validerfuntion);
-
-// //}
