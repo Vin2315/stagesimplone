a1208f616c5d02684021346495320e6f57066a50
Thibault RIZZO <thib.rizzo@gmail.com>
2022-01-15 09:16:34 +0200

    Add composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-develop',
+        'version' => 'dev-develop',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'b40f4192e757cd0668f75a901b2e91558bab8c5c',
+        'name' => 'vini/stagesimplone',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'vini/stagesimplone' => array(
+            'pretty_version' => 'dev-develop',
+            'version' => 'dev-develop',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'b40f4192e757cd0668f75a901b2e91558bab8c5c',
+            'dev_requirement' => false,
+        ),
+    ),
+);
